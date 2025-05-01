47b8580ce07d8e541c8a95e0e16c5ac7a91d138b
codergirlprerna <prernakhanna2602@gmail.com>
2025-05-02 03:58:33 +0530

    init

--- src/main/java/com/pm/patientService/controller/PatientController.java
+++ src/main/java/com/pm/patientService/controller/PatientController.java
@@ -1,13 +1,13 @@
 package com.pm.patientService.controller;
 
 
+import com.pm.patientService.dto.PatientRequestDTO;
 import com.pm.patientService.dto.PatientResponseDTO;
 import com.pm.patientService.model.Patient;
 import com.pm.patientService.service.PatientService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +23,13 @@ public class PatientController {
     public ResponseEntity<List<PatientResponseDTO>> getPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO= patientService.createPatient(patientRequestDTO);
+
+        return ResponseEntity.ok().body(patientResponseDTO);
     }
 
 }
--- src/main/java/com/pm/patientService/service/PatientService.java
+++ src/main/java/com/pm/patientService/service/PatientService.java
@@ -2,12 +2,20 @@ package com.pm.patientService.service;
 
 import com.pm.patientService.dto.PatientRequestDTO;
 import com.pm.patientService.dto.PatientResponseDTO;
+import com.pm.patientService.exception.EmailAlreadyExistsException;
+import com.pm.patientService.exception.PatientNotFoundException;
 import com.pm.patientService.mapper.PatientMapper;
 import com.pm.patientService.model.Patient;
 import com.pm.patientService.repository.PatientRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -23,9 +31,26 @@ public class PatientService {
        }
 
        public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("A Patient with this email" + "already exits" + patientRequestDTO.getEmail());
+        }
         Patient newPatient=patientRepository.save(
         PatientMapper.toModel(patientRequestDTO));
 
         return PatientMapper.toDTO(newPatient);
+       }
+       public PatientResponseDTO updatePatient(UUID id,PatientRequestDTO patientRequestDTO){
+        Patient patient=patientRepository.findById(id).orElseThrow(()-> new PatientNotFoundException("Patient not found with id:",id));
+           if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+               throw new EmailAlreadyExistsException("A Patient with this email" + "already exits" + patientRequestDTO.getEmail());
+           }
+           patient.setName(patientRequestDTO.getName());
+           patient.setAddress(patientRequestDTO.getAddress());
+           patient.setEmail(patientRequestDTO.getEmail());
+           patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+           Patient updatedPatient=patientRepository.save(patient);
+
+
        }
 }
