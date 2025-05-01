5e5efe0ad5aa67751b03a27825bb94ab3c90bdaa
codergirlprerna <prernakhanna2602@gmail.com>
2025-05-02 03:57:23 +0530

    init

---
+++ src/main/java/com/pm/patientService/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.pm.patientService.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
